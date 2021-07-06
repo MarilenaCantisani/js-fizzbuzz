0a38e5856b900ac3c8db0543bc8fbae7375629b2
MarilenaCantisani <marilena.cantisani97@gmail.com>
2021-07-06 09:53:15 +0200

    + Added innerHtml to ciclo for

--- js/script.js
+++ js/script.js
@@ -21,7 +21,23 @@ for (var i = 1; i <= 100; i++) {
 }
 
 
+var listElement = document.getElementById("list");
 
+var listItems = "";
 
+for (var i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        listItems += "<li>FizzBuzz</li>";
+    } 
+    else if (i % 3 == 0) {
+        listItems += "<li>Fizz</li>";
+    }
+    else if (i % 5 == 0) {
+        listItems += "<li>Buzz</li>";
+    }
+    else {
+        listItems += "<li>" + i + "</li>";
+    }   
+}
 
-
+listElement.innerHTML = listItems;
